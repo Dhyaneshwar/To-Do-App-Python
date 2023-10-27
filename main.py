ead8e419aa83bed55190db1d6bfd6cb03cb966e5
Dhyaneshwar N <dhyaneshwar98@gmail.com>
2023-10-27 12:14:30 +0100

    feat: Adding a new feature to mark a to-do as completed

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 default_todos = []
-run_loop = True
+
 while True :
-    user_msg = input("Enter a add, edit, show or exit:- ")
-    print('Value entered by the user is {}'.format(user_msg))
+    user_msg = input("Enter add, show, edit or exit:- ")
+    user_msg = user_msg.strip()     # used to remove leading and trailing whitespace
 
     match user_msg:
         case 'add':
-            user_input = input("Enter a todo:- ")
+            user_input = input("Enter a todo:- ").strip()
             default_todos.append(user_input)
+        case "show":
+            print("Todos are:-")
+            for (index, todo) in enumerate(default_todos):
+                print(f'{index + 1}--{todo}')
         case 'edit':
             user_input = int(input("Enter the index:- "))
-            default_todos[user_input] = input("Enter the edited todo:- ")
-        case "show":
-            print("todos are:-")
-            for (index, todo) in enumerate(default_todos):
-                print(f'{index + 1}.{todo}')
+            default_todos[user_input-1] = input("Enter the edited todo:- ")
+        case "complete":
+            user_input = int(input("Enter the completed index:- "))
+            default_todos.pop(user_input-1)
         case "exit":
-            print("---Thank you---")
+            print("---Thank you--- \n")
             break
         case _:
             print("Wrong input, Please try again")
+    print('\n')
